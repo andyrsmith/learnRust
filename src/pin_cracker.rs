0917f945daddec2e34d2b069abdcdb030f9870b3
Andy Smith <smith780@gmail.com>
2014-10-06 23:01:50 -0400

    Working with Tuples
    Can declare multiple varibles on one line like this let (x, y) = (1i,
    2i). Can also declare one varible with multiple values like this let x =
    (1i,2i,3i)

--- src/pin_cracker.rs
+++ src/pin_cracker.rs
@@ -2,7 +2,16 @@
 extern crate time;
 fn main(){
   //i means integer
-  let code = 111995i;
+  let (code, five) = (111995i, 5i);
+  
+  let x = (1i,2i,3i);
+  let y = (1i,2i,3i);
+
+  if x == y {
+    println!("A match!");
+  } else {
+    println!("nope");
+  }
 
   let start = time::precise_time_s();
   crack_pin(code, 6);
@@ -14,8 +23,11 @@ fn main(){
   println!("Looks like it can be cracked in {} seconds ", elapsed);
  
   //returns either 10 or 5 and stores it in variable y
-  let y = if code < 10000 { 10i } else { 5i };
+  let y = if five < 10 { 10i } else { 5i };
   println!("Look at this number {}", y);
+
+  let (thing1, thing2) = next_two(5i);
+  println!("thing1, thing2 = {}, {}", thing1, thing2);
 }
 
 fn crack_pin(x: int, y: int){
@@ -30,6 +42,7 @@ fn add_one(x: int) -> int {
   x + 1
 }
 
+fn next_two(x: int) -> (int, int) { (x + 1i, x+2i) }
 fn print_something(code: int){
   if code < 1000 {
     println!("Wow, that is pretty weak");
