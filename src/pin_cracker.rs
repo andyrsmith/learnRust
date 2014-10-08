67f1809d1348281f7e49e765ba5a62685690fa2f
Andy Smith <smith780@gmail.com>
2014-10-07 22:32:07 -0400

    Adds a Enum

--- src/pin_cracker.rs
+++ src/pin_cracker.rs
@@ -1,9 +1,15 @@
 ///Takes a random number which it calls a pin, and then tells you how fast it will take to crack it.
 extern crate time;
+
 //look a struct
 struct Point {
   x: int,
   y: int,
+}
+
+enum OptionalInt {
+  Value(int),
+  Missing,
 }
 
 fn main(){
@@ -12,15 +18,18 @@ fn main(){
   
   let first_match = (1i,2i,3i);
   let second_match = (1i,2i,3i);
-  //let's assign values from a struct
-  let point = Point {x: 1i, y: 6i};
-  //calling value from struct
-  println!("The points are at {} and {}", point.x, point.y);
+
   if first_match == second_match {
     println!("A match!");
   } else {
     println!("nope");
   }
+
+
+  //let's assign values from a struct
+  let point = Point {x: 1i, y: 6i};
+  //calling value from struct
+  println!("The points are at {} and {}", point.x, point.y);
 
   let start = time::precise_time_s();
   crack_pin(code, 6);
@@ -37,6 +46,18 @@ fn main(){
 
   let (thing1, thing2) = next_two(5i);
   println!("thing1, thing2 = {}, {}", thing1, thing2);
+
+  let real_value = Value(5);
+  let fake_value = Missing;
+
+  match real_value {
+    Value(n) => println!("x is {:d}", n),
+    Missing => println!("x is missing"),
+  }
+  match fake_value {
+    Value(n) => println!("x ix {:d}", n),
+    Missing => println!("x is missing"),
+  }
 }
 
 fn crack_pin(x: int, y: int){
